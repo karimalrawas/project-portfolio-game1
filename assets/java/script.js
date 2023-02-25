d4c251b3c2f81a7a4ab5c42929bdbb1ba4020ba4
karimalrawas <kemonu1@gmail.com>
2023-02-25 18:56:20 +0000

    edit css styling, padding, and margin

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,5 +1,5 @@
 
-let userScore = 0;
+let humanityScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
